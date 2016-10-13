6f71b516ae23900c8894f07fba6cbccdc7723a23
jyoung <violentjy@gmail.com>
2016-10-13 20:12:41 +0900

    HySONTest.java Junit test class 추가

---
+++ hySON-TEST/src/com/osori/hyson/HySONTest.java
@@ -0,0 +1,14 @@
+package com.osori.hyson;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HySONTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
