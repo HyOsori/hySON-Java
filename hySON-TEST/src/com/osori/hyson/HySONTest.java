97f5b3616ae5824e13294bc7714fd713c161694b
Kanak <kanak87@gmail.com>
2016-10-24 17:19:34 +0900

    add parse string array

--- hySON-TEST/src/com/osori/hyson/HySONTest.java
+++ hySON-TEST/src/com/osori/hyson/HySONTest.java
@@ -5,10 +5,48 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class HySONTest {
+	
+	@Test
+	public void tesParseEmptyStringArray() {
+		String jsonString = "[]";
+		HySON hySon = new HySON();
+		hySon.parse(jsonString);
+		
+		String[] results = hySon.getArrayString(jsonString);
+		assertEquals(0, results.length);
+	}
+	
+	@Test
+	public void tesParseStringArray() {
+		String jsonString = "[\"a\", \"b\", \"c\"]}";
+		HySON hySon = new HySON();
+		hySon.parse(jsonString);
+		
+		String[] results = null;
+
+		results = hySon.getArrayString(jsonString);
+		
+		assertEquals(3, results.length);
+		assertEquals("a", results[0]);
+		assertEquals("b", results[1]);
+		assertEquals("c", results[2]);
+	}
+	
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testParseWithConstructor() {
+		String jsonString = "[\"a\", \"b\", \"c\"]}";
+		HySON hySon = new HySON(jsonString);
+		
+		String[] results = null;
+
+		results = hySon.getArrayString(jsonString);
+		
+		assertEquals(3, results.length);
+		assertEquals("a", results[0]);
+		assertEquals("b", results[1]);
+		assertEquals("c", results[2]);
 	}
+
 
 }
--- hySON/src/com/osori/hyson/HySON.java
+++ hySON/src/com/osori/hyson/HySON.java
@@ -1,5 +1,34 @@
 package com.osori.hyson;
+import org.json.*;
 
 public class HySON {
+	
+	private JSONArray m_jsonArray = null;
+	
+	public HySON()
+	{
+	}
+	
+	public HySON(String jsonString)
+	{
+		m_jsonArray = new JSONArray(jsonString);
+	}
+
+	public String[] getArrayString(String jsonString)
+	{
+		String[] results = new String[m_jsonArray.length()];
+		
+		for(int i = 0; i < m_jsonArray.length(); ++i)
+		{
+			results[i] = m_jsonArray.getString(i);
+		}
+		
+		return results;
+	}
+	
+	public void parse(String jsonString)
+	{
+		m_jsonArray = new JSONArray(jsonString);
+	}
 
 }
