3494c07752eb67d52cad1a90f902fee4d5489a1e
Kanak <kanak87@gmail.com>
2017-01-04 00:13:08 +0900

    Android Studio에서 Module import되도록 수정
    Android에서 JSONObject 생성시 JSONException 에러가 발생한다.  Eclipse에서 개발할 때 사용된  json 라이브러리 버전에서 발생하지 않던 예외이다. json 에서 발생하는 exception을 try-catch로 잡았고, 이와 관련된 코드가 수정되었다.
    build.gradle이 추가되어 Android에서 Module import가 가능해졌다.

--- hySON/src/com/osori/hyson/HySON.java
+++ hySON/src/com/osori/hyson/HySON.java
@@ -30,48 +30,13 @@ public class HySON {
 		}
 	};
 
-	/** parse String Array **/
-	public static String[] getArrayString(String jsonString) {
-		JSONArray m_jsonArray = new JSONArray(jsonString);
-		String[] results = new String[m_jsonArray.length()];
-
-		for (int i = 0; i < m_jsonArray.length(); ++i) {
-			results[i] = m_jsonArray.getString(i);
-		}
-
-		return results;
-	}
-
-	/** parse Boolean Array **/
-	public static Boolean[] getArrayBoolean(String jsonString) {
-		JSONArray m_jsonArray = new JSONArray(jsonString);
-		Boolean[] results = new Boolean[m_jsonArray.length()];
-
-		for (int i = 0; i < m_jsonArray.length(); ++i) {
-			results[i] = m_jsonArray.getBoolean(i);
-		}
-
-		return results;
-	}
-
-	/** parse Integer Array **/
-	public static int[] getArrayInt(String jsonString) {
-		JSONArray m_jsonArray = new JSONArray(jsonString);
-		int[] results = new int[m_jsonArray.length()];
-
-		for (int i = 0; i < m_jsonArray.length(); i++) {
-			results[i] = m_jsonArray.getInt(i);
-		}
-
-		return results;
-	}
-
 	/** parse Custom Object **/
 	public static <T extends Object> T parse(String jsonString, Class c) {
-		JSONObject json = new JSONObject(jsonString);
 		T obj = null;
+		JSONObject json = null;
 
 		try {
+			json = new JSONObject(jsonString);
 			// get constructor
 			Constructor<?> constructor = c.getConstructor();
 
@@ -80,7 +45,8 @@ public class HySON {
 
 		} catch (NoSuchMethodException | SecurityException | 
 				InstantiationException | IllegalAccessException |
-				IllegalArgumentException | InvocationTargetException e) {
+				IllegalArgumentException | InvocationTargetException |
+				JSONException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 			
@@ -98,14 +64,6 @@ public class HySON {
 				field = c.getField(key);
 
 				if (isJSONUnit(field.getType())) {
-					// TODO : Double -> float/Floa 예외처리, Double -> String 등도 수정해야함
-//					if (field.getType() == float.class) {
-//						field.set(obj, ((Double) json.get(key)).floatValue());
-//					} else if (field.getType() == Float.class) {
-//						field.set(obj, ((Double) json.get(key)).floatValue());
-//					} else {
-//						field.set(obj, json.get(key));
-//					}
 					field.set(obj, stringToObject(field.getType(), json.optString(key)));
 				} else if (field.getType() == java.util.Date.class) {
 					field.set(obj, stringToDate(json.getString(key)));
@@ -141,24 +99,42 @@ public class HySON {
 	}
 
 	/** parse Custom Object With Key **/
-	public <T extends Object> T parse(String jsonString, String key, Class c) {
-		JSONObject json = new JSONObject(jsonString);
+	public <T> T parse(String jsonString, String key, Class c) {
+		JSONObject json = null;
+		try {
+			json = new JSONObject(jsonString);
+		} catch (JSONException e) {
+			return null;
+		}
+
 		jsonString = json.optString(key);
 
 		return parse(jsonString, c);
 	}
 
 	/** parse Custom Object Array **/
-	public static Object parseArray(String jsonString, Class c) {
-		JSONArray m_jsonArray = new JSONArray(jsonString);
+	private static Object parseArray(String jsonString, Class c) {
+		JSONArray m_jsonArray = null;
+
+		try {
+			m_jsonArray = new JSONArray(jsonString);
+		} catch (JSONException e) {
+			return null;
+		}
 
 		Object array = Array.newInstance(c, m_jsonArray.length());
 
 		for (int i = 0; i < m_jsonArray.length(); i++) {
-			try {
-				Array.set(array, i, parse(m_jsonArray.optString(i), c));
-			} catch (JSONException e) {
-				Array.set(array, i, m_jsonArray.get(i));
+			Object elementObject = parse(m_jsonArray.optString(i), c);
+			if (elementObject != null)
+				Array.set(array, i, elementObject);
+			else {
+				try {
+					elementObject = m_jsonArray.get(i);
+					Array.set(array, i, elementObject);
+				} catch (JSONException e) {
+					Array.set(array, i, null);
+				}
 			}
 		}
 
@@ -166,19 +142,35 @@ public class HySON {
 	}
 
 	/** parse Custom Object ArrayList **/
-	public static <T extends Object> ArrayList<T> parseArrayList(String jsonString, String key, Class c) {
-		JSONObject json = new JSONObject(jsonString);
+	private static <T> ArrayList<T> parseArrayList(String jsonString, String key, Class c) {
+		JSONObject json = null;
+		try {
+			json = new JSONObject(jsonString);
+		} catch (JSONException e) {
+			return null;
+		}
 		jsonString = json.optString(key);
 
-		JSONArray m_jsonArray = new JSONArray(jsonString);
+		JSONArray m_jsonArray = null;
+		try {
+			m_jsonArray = new JSONArray(jsonString);
+		} catch (JSONException e) {
+			return null;
+		}
 		ArrayList<T> array = new ArrayList<>();
 
 		for (int i = 0; i < m_jsonArray.length(); i++) {
-			try {
-				array.add(parse(m_jsonArray.optString(i), c));
-			} catch (JSONException e) {
-				array.add((T) m_jsonArray.get(i));
-			}
+
+			T elementObject = parse(m_jsonArray.optString(i), c);
+			if (elementObject != null)
+				array.add(elementObject);
+			else
+				try {
+					elementObject = (T)m_jsonArray.get(i);
+					array.add(elementObject);
+				} catch (JSONException e) {
+					array.add(null);
+				}
 		}
 
 		return array;
@@ -216,7 +208,7 @@ public class HySON {
 	}
 
 	private static java.util.Date stringToDate(String dateStr) {
-		SimpleDateFormat format = new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
 		try {
 			return format.parse(dateStr);
